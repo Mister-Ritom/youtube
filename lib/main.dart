5cc85fbd233c0d8df20ccf7846da19cec2439db0
Ritom <ritomghosh856@gmail.com>
2023-08-18 14:38:59 +0530

    Firebase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube/nav_pages/home.dart';
 import 'package:youtube/nav_pages/library.dart';
 import 'package:youtube/nav_pages/notifications.dart';
 import 'package:youtube/nav_pages/subscriptions.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
 }
 
